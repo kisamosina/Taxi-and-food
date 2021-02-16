a8d8a7d5cea1dbd713367bf6a7476c5490aa7b15
Daria <dashamosinaa@yandex.ru>
2021-02-16 19:01:37 +0800

    добавлен текст для перпезентации элементов

---
+++ Taxi and food/Data/PhotosViewControllerUIData.swift
@@ -0,0 +1,30 @@
+//
+//  PhotosViewControllerUIData.swift
+//  Taxi and food
+//
+//  Created by mac on 16/02/2021.
+//  Copyright © 2021 kisamosina. All rights reserved.
+//
+
+import Foundation
+
+struct PhotosViewControllerTextData {
+
+    internal enum RusLabelsTexts: String {
+    case selectImgeLabelTextRu = "При необходимости прикрепите изображения"
+}
+    internal enum EngLabelTexts: String {
+    case selectImgeLabelTextEn = "Attach images if necessary"
+    
+    }
+    
+    internal enum RusButtonTexts: String {
+        case sendButtonTextRu = "Отправить"
+        
+    }
+    
+    internal enum EngButtonTexts: String {
+        case sendButtonTextEn = "Send"
+        
+    }
+}
---
+++ Taxi and food/Data/ServiceViewControllerUIData.swift
@@ -0,0 +1,31 @@
+//
+//  ServiceViewControllerUIData.swift
+//  Taxi and food
+//
+//  Created by mac on 16/02/2021.
+//  Copyright © 2021 kisamosina. All rights reserved.
+//
+
+import Foundation
+
+struct ServiceViewControllerTextData {
+    
+    internal enum RusTextViewText: String {
+        case selectImgeLabelTextRu = "Опишите возникшую проблему"
+        
+    }
+    internal enum EngTextViewText: String {
+        case selectImgeLabelTextEn = "Describe your problem"
+    }
+    
+    internal enum RusButtonText: String {
+        case sendButtonTextRu = "Далее"
+        
+    }
+    
+    internal enum EngButtonText: String {
+        case sendButtonTextEn = "Next"
+        
+    }
+ 
+}
