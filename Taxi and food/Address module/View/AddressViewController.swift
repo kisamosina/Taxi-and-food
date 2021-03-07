bfcdaf7a931636e031a2de73d37cf3b8938427d5
Daria <dashamosinaa@yandex.ru>
2021-03-07 11:54:12 +0800

    added Address.storyboard

--- Taxi and food/Address module/View/AddressViewController.swift
+++ Taxi and food/Address module/View/AddressViewController.swift
@@ -10,10 +10,28 @@ import UIKit
 
 class AddressViewController: UIViewController {
 
+    @IBOutlet var nameTextView: UITextView!
+    @IBOutlet var addressTextView: UITextView!
+    @IBOutlet var commentTextView: UITextView!
+    
+    @IBOutlet var officeTextView: UITextView!
+    @IBOutlet var entranceTextView: UITextView!
+    
+    @IBOutlet var intercomTextView: UITextView!
+    
+    @IBOutlet var floorTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureTextViews()
 
       
+    }
+    
+    func configureTextViews() {
+        self.nameTextView.text = "Название адреса"
+        
     }
 
 }
