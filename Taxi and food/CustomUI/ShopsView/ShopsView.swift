b8eddaf4fc54959bcdd76f1c2ccdd25107b86c86
Maksim Alekseev <piratmax@gmail.com>
2021-03-31 21:44:06 +0300

    added shops view

---
+++ Taxi and food/CustomUI/ShopsView/ShopsView.swift
@@ -0,0 +1,70 @@
+//
+//  ShopsView.swift
+//  Taxi and food
+//
+//  Created by Maxim Alekseev on 31.03.2021.
+//  Copyright © 2021 kisamosina. All rights reserved.
+//
+
+import UIKit
+
+class ShopsView: UIView {
+    
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var placeLabel: UILabel!
+    @IBOutlet weak var placeAddressLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    //MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.initSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.initSubviews()
+    }
+
+    
+    //MARK: - Methods
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        //Layout setup
+        self.contentView.layer.cornerRadius = TransitionBottomViewSizes.cornerRadius.rawValue
+        self.contentView.layer.shadowColor = Colors.shadowColor.getColor().cgColor
+        self.contentView.layer.shadowOpacity = Float(TransitionBottomViewSizes.shadowOpacity.rawValue)
+        self.contentView.layer.shadowRadius = TransitionBottomViewSizes.cornerRadius.rawValue
+        self.contentView.layer.shadowOffset = CGSize(width: TransitionBottomViewSizes.shadowOffsetWidth.rawValue,
+                                                     height: TransitionBottomViewSizes.shadowOffsetWidth.rawValue)
+        self.contentView.layer.masksToBounds = false
+        self.topView.layer.cornerRadius = self.topView.frame.height/2
+        self.topView.clipsToBounds = true
+        
+    }
+    
+    private func initSubviews() {
+        
+        self.loadFromNib(nibName: ShopsViewNibsNames.ShopsView.rawValue)
+        self.containerView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(containerView)
+        self.setupConstraints()
+
+    }
+
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            self.topAnchor.constraint(equalTo: containerView.topAnchor),
+            self.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            self.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            self.trailingAnchor.constraint(equalTo: containerView.trailingAnchor)
+        ])
+    }
+}
---
+++ Taxi and food/Data/ShopsViewUIData.swift
@@ -0,0 +1,13 @@
+//
+//  ShopsViewUIData.swift
+//  Taxi and food
+//
+//  Created by Maxim Alekseev on 31.03.2021.
+//  Copyright © 2021 kisamosina. All rights reserved.
+//
+
+import Foundation
+
+enum ShopsViewNibsNames: String {
+    case ShopsView
+}
