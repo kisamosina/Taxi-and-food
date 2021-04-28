17f16d745eb1b57a3722b342bde2eb907f057a55
Maksim Alekseev <piratmax@gmail.com>
2021-04-28 08:05:35 +0300

    fix

---
+++ Taxi and food/Data/DiscountsButtonViewUIData.swift
@@ -0,0 +1,21 @@
+//
+//  DiscountsButtonViewUIData.swift
+//  Taxi and food
+//
+//  Created by Maxim Alekseev on 27.04.2021.
+//  Copyright © 2021 kisamosina. All rights reserved.
+//
+
+import UIKit
+
+enum DiscountsButtonViewStringData: String {
+    case nibFileName = "DiscountsButtonView"
+}
+
+enum DiscountsButtonViewSizeData: CGFloat {
+    case shadowOpacity = 1
+    case shadowRadius = 10
+    case cornerRadius = 15
+    case shadowOffsetHeight = 1.01
+    case shadowOffsetWidth = 1.02
+}
---
+++ Taxi and food/Data/TaxiOrderViewUIData.swift
@@ -0,0 +1,26 @@
+//
+//  TaxiOrderViewUIData.swift
+//  Taxi and food
+//
+//  Created by Maxim Alekseev on 27.04.2021.
+//  Copyright © 2021 kisamosina. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum TaxiOrderViewStringData: String {
+    case nibFileName = "TaxiOrderView"
+    case tariffsCollectionViewCellReuseId = "TaxiOrderCollectionViewCell"
+}
+
+enum TaxiOrderViewSizesData: CGFloat {
+    case viewHeight = 450
+    case tariffCellShadowOpacity = 1
+    case tariffCellShadowRadius = 15
+    case tariffCellShadowOffsetWidth = 1.01
+    case tariffCellShadowOffsetHeight = 1.02
+    case collectionViewSumOffsets = 64
+    case tariffCellHeight = 80
+}
+
